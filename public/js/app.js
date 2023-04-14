374e58018acfb86a1e3424f96e22aba651da7191
shansach <mail.shantanusachdeva@gmail.com>
2023-04-13 20:52:14 -0400

    Wiring UI test 1

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,10 @@
-fetch('/weather?address=Boston').then((response)=>{
-    response.json().then((data)=>{
-
-        if(data.error)
-        {
-            console.log(data.error);
-        }
-        else
-        {
-            console.log(data.location + " " + data.weather_description);
-        }
-    })
-})
-
 const wform = document.querySelector('form');
 const lSearch = document.querySelector('input');
+const m1 = document.querySelector('#m1');
+const m2 = document.querySelector('#m2');
+
+m1.innerText = "Hey this is the frontend js"
+
 
 wform.addEventListener('submit',(event)=>{
 
